56f835fca5c4f808de9f8239481f754edeaa1a09
Maxim Ignatenko <imax@cesanta.com>
2015-05-30 19:16:03 +0100

    Update CLI util

--- main.go
+++ main.go
@@ -6,18 +6,20 @@ import (
 	"io/ioutil"
 	"os"
 
+	json "github.com/cesanta/ucl"
 	"github.com/cesanta/validate-json/schema"
 )
 
 var (
 	schemaFile = flag.String("schema", "", "Path to schema to use.")
+	inputFile  = flag.String("input", "", "Path to the JSON data to validate.")
 )
 
 func main() {
 	flag.Parse()
 
-	if *schemaFile == "" {
-		fmt.Fprintf(os.Stderr, "Need --schema\n")
+	if *schemaFile == "" || *inputFile == "" {
+		fmt.Fprintf(os.Stderr, "Need --schema and --input\n")
 		os.Exit(1)
 	}
 
@@ -27,9 +29,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = schema.ParseDraft04Schema(b)
+	s, err := schema.ParseDraft04Schema(b)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Schema is not valid: %s\n", err)
+		os.Exit(1)
+	}
+
+	validator := schema.NewValidator(s)
+
+	f, err := os.Open(*inputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to open input file: %s", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	data, err := json.Parse(f)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse input file: %s", err)
+		os.Exit(1)
+	}
+	if err := validator.Validate(data); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
